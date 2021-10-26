d7bd7d07c734ad51824c7eaf9a60486ddadaf40a
Filipe Voges <filipe.vogesh@gmail.com>
2021-10-26 08:39:29 -0300

    Suna Framework Start

---
+++ README.md
@@ -0,0 +1,3 @@
+# Suna
+
+👪 Suna Framework - innovative framework for fast and easy development of secure web applications in PHP.
---
+++ Suna/Framework.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1);
+
+namespace Suna;
+
+/**
+ * Framework version.
+ *
+ * @author Filipe Voges <filipe.vogesh@Gmail.com
+ * @since 2021-10-26
+ */
+class Framework
+{
+    /**
+     * Framework Name
+     *
+     * @var string
+     * @access public
+     */
+    public const NAME = 'Suna Framework';
+
+    /**
+     * Framework Version
+     *
+     * @var string
+     * @access public
+     */
+    public const VERSION = '0.1';
+
+    /**
+     * Framework Version Id
+     *
+     * @var int
+     * @access public
+     */
+    public const VERSION_ID = 00100;
+
+    /**
+     * Framework Revision
+     *
+     * @var string
+     * @access public
+     */
+    public const REVISION = '';
+}
